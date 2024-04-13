a8134f40b74807ed9872632aeb464faf1c1daa86
bahar <101214031+baharraksu@users.noreply.github.com>
2024-04-13 15:22:18 +0300

    Update mysql.js

--- technical-assignment1/helper/mysql.js
+++ technical-assignment1/helper/mysql.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const connection = mysql.createPool({
   connectionLimit: 100,
   host: "localhost",
-  user: "root",
-  password: "Key45031*",
+  user: "",
+  password: "",
   database: "work_app",
 });
 
